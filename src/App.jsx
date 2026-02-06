328403598190bbe810d1ba25a4d8a243586bc7dd
Muhammadqodir779 <kadirovmuhammadqodir663@gmail.com>
2026-02-06 12:17:39 +0500

    SignUp qo'shildi

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import './App.css';
 import Mashhura from './components/mashhura/Mashhura';
 import SignIn from './components/muhammadqodir/signIn/SignIn';
-import Muhammadyusuf from './components/muhammadyusuf/Muhammadyusuf';
-import Ikromjon from './components/ikromjon/Ikromjon';
+import RegisterStep1 from './components/ikromjon/registerStep1/Step1PersonalWrapper';
+import RegisterStep2 from './components/ikromjon/registerStep2/Step2EducationWrapper';
+import RegisterStep3 from './components/ikromjon/registerStep3/Step3PreferencesWrapper';
+import RegisterStep4 from './components/ikromjon/registerStep4/Step4FinishWrapper';
 import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
@@ -12,6 +14,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Mashhura />} />
         <Route path="/sign_in" element={<SignIn />} />
+        <Route path="/registerStep1" element={<RegisterStep1 />} />
+        <Route path="/registerStep2" element={<RegisterStep2 />} />
+        <Route path="/registerStep3" element={<RegisterStep3 />} />
+        <Route path="/registerStep4" element={<RegisterStep4 />} />
       </Routes>
       <Muhammadyusuf />
       <Ikromjon />
